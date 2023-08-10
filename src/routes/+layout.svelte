b795b5b301004ce4fea1933da8d8f06ba7a18957
nitishmalluru <nitishethan@gmail.com>
2023-08-10 01:10:44 -0500

    Added a sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
 	import type { LayoutData } from './$types';
+	import '../app.css';
 
 	export let data: LayoutData;
 
@@ -22,25 +23,27 @@
 	});
 </script>
 
-<nav>
-	<a href="/" class="btn btn-primary">Home</a>
-	{#if data.session}
-		<a href="/tutor" class="btn btn-secondary">Create A Class</a>
-		<a href="/logout">Logout</a>
-		{#if data.profile}
-			<div style="float: right; display: flex; align-items: center;">
-				<p style="margin-right: 10px;">{data.profile[0].first_name} {data.profile[0].last_name}</p>
-				<img
-					src={data.profile[0].avatar_url}
-					alt="Profile Image"
-					style="width: 50px; height: 50px; border-radius: 50%;"
-				/>
+{#if data.session}
+	<div class="flex">
+		<div class="h-screen flex flex-col justify-between w-1/12 bg-gray-200">
+			<div class="flex flex-col items-center p-2 text-center">
+				<a href="/" class="w-full py-3 text-gray-900 rounded-lg hover:bg-gray-100"> Home </a>
+				<a href="/tutor" class="w-full py-3 text-gray-900 rounded-lg hover:bg-gray-100"> Create </a>
+				<a href="/logout" class="w-full py-3 text-gray-900 rounded-lg hover:bg-gray-100">
+					Log Out
+				</a>
 			</div>
-		{/if}
-	{:else}
-		<a href="/login" class="btn btn-primary">Login</a>
-		<a href="/signup" class="btn btn-secondary">Sign Up</a>
-	{/if}
-</nav>
-
-<slot />
+			<img
+				src={data.profile[0].avatar_url}
+				alt="Profile Picture"
+				class="mx-auto w-16 rounded-full hover:w-20 duration-75"
+			/>
+		</div>
+		<slot />
+	</div>
+{:else}
+	<a href="/" class="btn btn-primary">Home</a>
+	<a href="/login" class="btn btn-primary">Login</a>
+	<a href="/signup" class="btn btn-secondary">Sign Up</a>
+	<slot />
+{/if}
